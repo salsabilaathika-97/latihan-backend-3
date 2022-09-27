aa80bb8bf705c927203100b588dc55f632e815b4
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-27 20:48:47 +0800

    add api get

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express = require("express")
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res("Halo, saya bukan bjorka")
+})
+
+const PORT = 1001;
+
+app.listen(PORT, ()=> {
+    console.log(`Running on port ${PORT}`);
+})
