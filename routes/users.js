e29dcc4eda323d48ca8d81e6cb0e669dd2fd81a5
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-29 21:55:18 +0800

    create get data by id from database

--- routes/users.js
+++ routes/users.js
@@ -15,4 +15,16 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+    const dataParam = req.params;
+    const sqlQuery = `SELECT * FROM user WHERE user_id = ${dataParam.id}`
+    db.query(sqlQuery, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result)
+        }
+    });
+});
+
 module.exports = router;
